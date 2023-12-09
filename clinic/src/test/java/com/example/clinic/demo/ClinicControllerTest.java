942aa767eb149b1812ac6468feb0234cb94f6ed1
PrivateCzopik <czopikmateusz@gmail.com>
2023-12-09 15:15:30 +0100

    Add: Initialize clinic controller test class

---
+++ clinic/src/test/java/com/example/clinic/demo/ClinicControllerTest.java
@@ -0,0 +1,30 @@
+package com.example.clinic.demo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(ClinicController.class)
+class ClinicControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ClinicService clinicService;
+
+    
+}
